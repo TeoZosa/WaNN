57c6a8e822715d9f1f183105b1e6428cf79a4ebb
TeoZosa <erinzosa@ucla.edu>
2017-01-14 10:11:42 -0800

    cleaned up code

--- Tools/NumpyArray.py
+++ Tools/NumpyArray.py
@@ -120,32 +120,23 @@ def SplitArraytoXMatrixAndYTransitionVector(arrayToSplit, cnn_format=True):
         for plane in trainingExample[0]:
             x += plane #flatten 2d matrix
         x += [1] * 64 # 1 bias plane
+        # one_hot_feature_indexes = [index for index, has_piece in enumerate(x) if has_piece == 1]
+
+        # if we just want the transition index vs a pre-one-hotted vector
+        # one_hot_transition_index = trainingExample[2].index(1) # assumes no errors; only one value of 1
+
         if cnn_format:
             x = np.reshape(np.array(x, dtype=np.float32), (len(x) // 64, 8, 8))  # feature_plane x row x co)
             for i in range(0, len(x)):
                 x[i] = x[i].transpose() #transpose (row x col) to get feature_plane x col x row
             x = x.transpose()#  transpose to get proper dimensions (row x cols  x feature plane)
-        # one_hot_indices = []
-        # for position_index in range(0, len(x)):  # pass indices to tf.one_hot
-        #     if x[position_index] == 1:
-        #         one_hot_indices.append(position_index)
-        one_hot_transitions = None
-        transition_vector = np.array(trainingExample[2], dtype=np.float32)
-        if not transition_vector.any():
-            transition_vector = np.array([0]*154 + [1], dtype=np.float32)
         else:
-            transition_vector = np.append(transition_vector, [0])
-        # for transition in range(0, len(transition_vector)):
-        #     if transition_vector[transition] == 1:
-        #         one_hot_transitions = transition
-        #         break
-        # if one_hot_transitions == None:  # no move made
-        #     # one_hot_transitions = -1  # tf.one_hot will return a vector of all 0s
-        #     one_hot_transitions = 154  # DNNClassifier => 154 == no move category
+            x = np.array(x, dtype=np.float32)  # 1d board
+
+        # One hot transition vector
+        transition_vector = np.array(trainingExample[2], dtype=np.float32)
         X.append(x)
         y.append(transition_vector)  # transition number
-        # X.append(np.array(x, dtype=np.int32)) #1d board
-        # y.append(trainingExample[2])#transition vector
     return X, y
 
 def SplitArraytoXMatrixAndYTransitionVectorCNN(arrayToSplit):  # only for boards with pd dataframe

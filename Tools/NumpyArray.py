37aceb347fa63a3c9b44d47fb7833a8365fc5902
TeoZosa <erinzosa@ucla.edu>
2017-01-25 09:21:00 -0800

    value net code and code cleanup

--- Tools/NumpyArray.py
+++ Tools/NumpyArray.py
@@ -66,31 +66,31 @@ def FilterByWinRatio(playerList):
     print ('# of States If We Filter by Win Ratio: {states}'.format(states = len(X)))
     return X
 
-def FilterForSelfPlay(selfPlayDataList, NNType):
-    X = []
-    if NNType == 'Policy':
-      for selfPlayLog in selfPlayDataList:
-          for game in selfPlayLog['Games']:
-              states = game['BoardStates']['PlayerPOV']
-              mirrorStates = game['MirrorBoardStates']['PlayerPOV']
-              assert len(states) == len(mirrorStates)
-              for i in range(0, len(states)):
-                  X.append(states[i])
-                  X.append(mirrorStates[i])
-      print('# of States for Self-Play Policy Net: {states}'.format(states=len(X)))
-    else:  # value net
-      for selfPlayLog in selfPlayDataList:
-          for game in selfPlayLog['Games']:
-              states = game['BoardStates']['States']
-              mirrorStates = game['MirrorBoardStates']['States']
-              assert len(states) == len(mirrorStates)
-              for i in range(0, len(states)):
-                  X.append(states[i])
-                  X.append(mirrorStates[i])
-      print('# of States for Self-Play Value Net: {states}'.format(states=len(X)))
-    return X
+def filter_for_self_play(self_play_data, NNType):
+    training_data = []
+    # if NNType == 'Policy':
+    for self_play_log in self_play_data:
+      for game in self_play_log['Games']:
+          states = game['BoardStates']['PlayerPOV']
+          mirror_states = game['MirrorBoardStates']['PlayerPOV']
+          assert len(states) == len(mirror_states)
+          for i in range(0, len(states)):
+              training_data.append(states[i])
+              training_data.append(mirror_states[i])
+      print('# of States for Self-Play {NNType} Net: {states}'.format(states=len(training_data), NNType=NNType))
+    # else:  # value net
+    #   for selfPlayLog in selfPlayDataList:
+    #       for game in selfPlayLog['Games']:
+    #           states = game['BoardStates']['States']
+    #           mirrorStates = game['MirrorBoardStates']['States']
+    #           assert len(states) == len(mirrorStates)
+    #           for i in range(0, len(states)):
+    #               X.append(states[i])
+    #               X.append(mirrorStates[i])
+    #   print('# of States for Self-Play Value Net: {states}'.format(states=len(X)))
+    return training_data
 
-def SplitArraytoXMatrixAndYVector(arrayToSplit, convertY = True):
+def SplitArraytoXMatrixAndYOutcomeVector(arrayToSplit, convertY = True):
     X = []
     y = []
     if (convertY == True):
@@ -112,31 +112,58 @@ def SplitArraytoXMatrixAndYVector(arrayToSplit, convertY = True):
     return X, y
 
 
-def SplitArraytoXMatrixAndYTransitionVector(arrayToSplit, cnn_format=True, one_hot_indexes=False):
-    X = []
-    y = []
-    for trainingExample in arrayToSplit:  # probability space for transitions
-        x = []
-        for plane in trainingExample[0]:
-            x += plane #flatten 2d matrix
-        x += [1] * 64 # 1 bias plane
-        if one_hot_indexes:
-            #x still a 1d array; must stay one-hotted to be reshaped properly
-            x = np.array([index for index, has_piece in enumerate(x) if has_piece == 1], dtype=np.float32)
-            # if we just want the transition index vs a pre-one-hotted vector
-            transition_vector = trainingExample[2].index(1) # assumes no errors, y has a single value of 1
-        elif cnn_format:
-            x = np.reshape(np.array(x, dtype=np.float32), (len(x) // 64, 8, 8))  # feature_plane x row x co)
-            for i in range(0, len(x)):
-                x[i] = x[i].transpose() #transpose (row x col) to get feature_plane x col x row
-            x = x.transpose()#  transpose to get proper dimensions: row x col  x feature plane
-            transition_vector = np.array(trainingExample[2], dtype=np.float32)  # One hot transition vector
-        else:
-            x = np.array(x, dtype=np.float32)  # 1d board
-            transition_vector = np.array(trainingExample[2], dtype=np.float32)  # One hot transition vector
-        X.append(x)
-        y.append(transition_vector)  # transition number
-    return X, y
+def split_data_to_training_examples_and_labels_for_CNN(array_to_split, NNType):
+    training_examples = []
+    labels = []
+    for training_example in array_to_split:  # probability space for transitions
+        formatted_example, formatted_label = split_training_examples_and_labels(training_example, NNType)
+        training_examples.append(formatted_example)
+        labels.append(formatted_label)  # transition number
+    return training_examples, labels
+
+def split_training_examples_and_labels(training_example, NNType):
+    formatted_example = format_training_example(training_example[0])
+    if NNType == 'Policy':
+         formatted_label = label_for_policy(label=training_example[2])
+    else: # Value Net
+         formatted_label = label_for_value(label=training_example[1])
+    return formatted_example, formatted_label
+
+def format_training_example(training_example):
+    formatted_example = []
+    for plane in training_example:
+        formatted_example += plane  # flatten 2d matrix
+    formatted_example += [1] * 64  # 1 bias plane
+    formatted_example = np.reshape(np.array(formatted_example, dtype=np.float32),
+                                   (len(formatted_example) // 64, 8, 8))  # feature_plane x row x co)
+    for i in range(0, len(formatted_example)):
+        formatted_example[i] = formatted_example[
+            i].transpose()  # transpose (row x col) to get feature_plane x col x row
+    formatted_example = formatted_example.transpose()  # transpose to get proper dimensions: row x col  x feature plane
+    return formatted_example
+    
+def label_for_policy(label, one_hot_indexes=False):
+    # if one_hot_indexes:
+    #     # x still a 1d array; must stay one-hotted to be reshaped properly
+    #     formatted_example = np.array([index for index, has_piece in enumerate(formatted_example) if has_piece == 1], dtype=np.float32)
+    #     # if we just want the transition index vs a pre-one-hotted vector
+    #     formatted_label = label.index(1)  # assumes no errors, y has a single value of 1
+    formatted_label = np.array(label, dtype=np.float32)  # One hot transition vector
+    return formatted_label
+
+def label_for_value(label): 
+    if label == -1:
+        formatted_label = 0
+    elif label == 1:
+        formatted_label = label
+    else:
+        formatted_label = None
+        print("Error: y[i] should be 1 or -1")
+        exit(-11)
+    return formatted_label
+
+    
+
 
 def SplitArraytoXMatrixAndYTransitionVectorCNN(arrayToSplit):  # only for boards with pd dataframe
     warnings.warn("Only for use with PD Dataframe data; "
@@ -164,44 +191,21 @@ def SplitArraytoXMatrixAndYTransitionVectorCNN(arrayToSplit):  # only for boards
         X.append(np.array(x, dtype=np.int32))
     return X, y
 
-def generateArray(playerListDataFriendly, filter, NNType):
+def generateArray(player_list, filter, NNType):
         if filter =='Win Ratio':
-            trainingExamples = FilterByWinRatio(playerListDataFriendly)
+            training_data = FilterByWinRatio(player_list)
         elif filter =='Rank':
-            trainingExamples = FilterByRank(playerListDataFriendly)
+            training_data = FilterByRank(player_list)
         elif filter =='Self-Play':
-            trainingExamples = FilterForSelfPlay(playerListDataFriendly, NNType)
+            training_data = filter_for_self_play(player_list, NNType)
         else:
-            trainingExamples=[]
+            training_data= None
             print('Invalid Filter Specified')
-            exit(-2)
-        # Xcopy = copy.deepcopy(trainingExamples)
-        # shuffle(trainingExamples)  # to break symmetry/prevent overfitting
-        # assert (Xcopy != trainingExamples)
-        #split into trainingExamples array and y vectors
-        #
-        if NNType =='Policy':
-            X, y = SplitArraytoXMatrixAndYTransitionVector(trainingExamples)
-        else: # Value Net
-            X, y = SplitArraytoXMatrixAndYVector(trainingExamples, convertY= True)
-        # trainingExamples = np.matrix(X, dtype=np.int8)
-
-        # transform to TrainingExamples x( m x 64) numpy array; 8-bit ints since legal values are -1, 0, 1 or 0, 1
-        X = np.array(X, dtype=np.float32)
-        # transform to m x 1 numpy array; 8-bit ints since legal values are -1, 1 or 0, 1
-        if NNType =='Policy1':
-            y = np.matrix(y, dtype=np.float32)
-        else:
-            y = np.array(y, dtype=np.float32)
-        # test to make sure y values map to corresponding row vectors in trainingExamples
-        # GenerateCSV(X)
-        # GenerateCSV(y, isX=False)
-
-        # for i in range(0, len(y)):
-        #     compare = X[i].tolist()
-        #     assert (trainingExamples[i][0] == compare[0])
-        #     assert (trainingExamples[i][1] == y[i] or y[i] == 0)
-        return X, y
+            exit(-2)       
+        training_examples, labels = split_data_to_training_examples_and_labels_for_CNN(training_data, NNType)
+        training_examples = np.array(training_examples, dtype=np.float32)
+        labels = np.array(labels, dtype=np.float32)
+        return training_examples, labels
 def GenerateCSV(X, isX = True):
     if isX == True:
      np.savetxt(r'/Users/TeofiloZosa/PycharmProjects/BreakthroughANN/ValueNetRankBinary/NPDataSets/WBPOEUnshuffledBinaryFeaturePlanesWBPOETrainingExamples.csv', X, delimiter=',', fmt='%1i')

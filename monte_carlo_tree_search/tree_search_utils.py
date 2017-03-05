fixed backpropagation bug, logic to choose child from UCT (minimaxing)

monte_carlo_tree_search/tree_search_utils.py
@@ -1,42 +1,37 @@
 from Breakthrough_Player.board_utils import generate_policy_net_moves_batch, generate_policy_net_moves
 import math
+import numpy as np
 
+#backpropagate wins
 def update_tree_wins(node, amount=1): #visits and wins go together
     node.wins += amount
     node.visits += amount
     parent = node.parent
-    while parent is not None:
-        grandparent = parent.parent
-        if grandparent is not None:
-            grandparent.wins += amount #since win for child = loss for parent = win for grandparent
-        parent.visits += amount #should correctly increment grandparent visits as well
-        parent = parent.parent
+    if parent is not None: #node win => parent loss
+        update_tree_losses(parent, amount)
 
-def update_tree_losses(node, amount=1): # visits and losses go together
+#backpropagate losses
+def update_tree_losses(node, amount=1): # visit and no win = loss
     node.visits += amount
     parent = node.parent
-    while parent is not None:
-        grandparent = parent.parent
-        if grandparent is not None:
-            grandparent.visits += amount
-        parent.wins += amount# since loss for child = win for parent = loss for grandparent
-        parent.visits += amount
-        parent = parent.parent
+    if parent is not None: #node loss => parent win
+        update_tree_wins(parent, amount)
 
 def choose_move(node):
     parent_visits = node.visits
-    best = None
-    best_val = -1  # child may have a 0 uct value if visited too much
+    worst = None
     if node.children is not None:
-        for child in node.children:
-            child_value = get_UCT(child, parent_visits)
-            if child_value > best_val:
-                best = child
-                best_val = child_value
-    return best
+        worst = node.children[0]
+        worst_val = get_UCT(node.children[0], parent_visits)
+        for i in range(1, len(node.children)):
+            child_value = get_UCT(node.children[i], parent_visits)
+            if child_value < worst_val:
+                worst = node.children[i]
+                worst_val = child_value
+    return worst  #because a win for me = a loss for child?
 
 def get_UCT(node, parent_visits):
-    return (node.wins / node.visits) + (1.414 * math.sqrt(math.log(parent_visits) / node.visits))
+    return np.float64((node.wins / node.visits) + (1.414 * math.sqrt(math.log(parent_visits) / node.visits)))
 
 def update_values_from_policy_net(game_tree):
     NN_output = generate_policy_net_moves_batch(game_tree)
@@ -50,7 +45,7 @@ def update_values_from_policy_net(game_tree):
                     weighted_wins = int(NN_output[i][child.index] * NN_weighting)
                     # num_extra_visits = weighted_wins
                     # update_tree_losses(child, num_extra_visits)
-                    update_tree_wins(child, weighted_wins)  # say we won every time we visited,
+                    update_tree_losses(child, weighted_wins)  # say we won (child lost) every time we visited,
                     # or else it may be a high visit count with low win count
 
 def update_single_value_from_policy_net(node):  # multi valued call should work too?
@@ -62,5 +57,5 @@ def update_single_value_from_policy_net(node):  # multi valued call should work 
             weighted_wins = int(NN_output[child.index] * NN_weighting)
             # num_extra_visits = weighted_wins
             # update_tree_losses(child, num_extra_visits)
-            update_tree_wins(child, weighted_wins)  # say we won every time we visited,
+            update_tree_losses(child, weighted_wins)  # say we won (child lost) every time we visited,
             # or else it may be a high visit count with low win count
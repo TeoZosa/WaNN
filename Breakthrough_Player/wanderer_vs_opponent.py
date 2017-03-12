5f3ac7e664150ce5b757f27dad3e427c0036631e
TeoZosa <erinzosa@ucla.edu>
2017-03-12 12:35:00 -0700

    fixed human v computer move. debugging pipe to play wanderer

--- Breakthrough_Player/wanderer_vs_opponent.py
+++ Breakthrough_Player/wanderer_vs_opponent.py
@@ -1,4 +1,3 @@
-import pexpect
 from Breakthrough_Player import breakthrough_player
 from multiprocessing import freeze_support
 
@@ -9,7 +8,7 @@ if __name__ == '__main__':#for Windows since it lacks os.fork
   num_games_to_play = 10
   time_to_think = 10
   depth_limit = 1
-  date = r'03102017'
+  date = r'03122017'
   file_designator = ''
   expansion_MCTS = 'Expansion MCTS'
   expansion_MCTS_pruning = 'Expansion MCTS Pruning'
@@ -26,21 +25,3 @@ if __name__ == '__main__':#for Windows since it lacks os.fork
   # path = r'G:\TruncatedLogs\PythonDatasets'
 
   breakthrough_player.play_game_vs_wanderer(expansion_MCTS_pruning, human, depth_limit, time_to_think)
-
-open_input_engine = r'\Users\TeofiloZosa\Clion\Breakthrough\BreakthroughInput\bin\Release\BreakthroughInput'
-color_to_be = r' white '
-wanderer_executable = r'\Users\TeofiloZosa\Clion\Breakthrough\BreakthroughCurrent\bin\Release\BreakthroughCurrent --ttt=10'
-
-child = pexpect.spawn(open_input_engine + color_to_be + wanderer_executable)
-child.expect('Name .*: ')
-child.sendline('anonymous')
-child.expect('Password:')
-child.sendline('noah@example.com')
-child.expect('ftp> ')
-child.sendline('lcd /tmp')
-child.expect('ftp> ')
-child.sendline('cd pub/OpenBSD')
-child.expect('ftp> ')
-child.sendline('get README')
-child.expect('ftp> ')
-child.sendline('bye')

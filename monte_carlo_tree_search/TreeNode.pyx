7d3e186189c61bd85e3980e92ab418dd5886a22f
TeoZosa <erinzosa@ucla.edu>
2017-06-21 21:52:56 -0700

    commented tree node

--- monte_carlo_tree_search/TreeNode.pyx
+++ monte_carlo_tree_search/TreeNode.pyx
@@ -1,95 +1,9 @@
 #cython: language_level=3, boundscheck=False
-# from cpython cimport bool
 from cpython.array cimport array
-
-# class TreeNode(object):
-#     def __init__(self, game_board, white_pieces, black_pieces, player_color, index, parent,  height):
-#         self['game_board'] = game_board
-#         self['color'] = player_color
-#         self['index'] = index #if None => root
-#         self['parent'] = parent#to pass up visits/wins if None => root
-#         self['white_pieces'] = white_pieces
-#         self['black_pieces'] = black_pieces
-#         self['children'] = None # all kids
-#         self['other_children'] = None #separate non_pruned tree
-#         self['win_status'] = None
-#         self['best_child'] = None #to expand best child first
-#         self['subtree_being_checked'] = False
-#         self['subtree_checked'] = False
-#
-#
-#         self['reexpanded_already'] = False
-#
-#
-#         self['gameover'] = False
-#
-#         self['visits'] = 0 #should be equal to sum of all children + num_times it was rolled out
-#         self['wins'] = 0 # 0 <= wins <= visits
-#
-#         self['gameover']_visits = 0
-#         self['gameover_wins'] = 0
-#
-#         self['height'] = height #if root is first state in game MCTS saw
-#
-#         self['threads_checking_node'] = 0
-#
-#
-#         self['UCT_multiplier'] = 1
-#         self['num_children_being_checked'] = 0
-#
-#
-#         self['overwhelming_amount'] = 1#65536
-#
-#         self['num_to_consider'] = 1
-#         self['num_to_keep'] = 1
-#         self['times_reexpanded'] = 0
-
-
-# cdef struct TreeNode:
-#     # def __init__(self, game_board, white_pieces, black_pieces, player_color, index, parent,  height):
-#     dict game_board = game_board
-#     str color = player_color
-#     int index = index #if None => root
-#     TreeNode parent = parent#to pass up visits/wins if None => root
-#     array[str] white_pieces = white_pieces
-#     array[str] black_pieces = black_pieces
-#     array[TreeNode] children # all kids
-#     array[TreeNode] other_children #separate non_pruned tree
-#     # enum win_status: 0 = 'None', 1 = 'True', 2 = 'False'
-#     TreeNode best_child #to expand best child first
-#     bool subtree_being_checked = False
-#     bool subtree_checked = False
-#
-#
-#     bool reexpanded_already = False
-#
-#
-#     bool gameover = False
-#
-#     int visits = 0 #should be equal to sum of all children + num_times it was rolled out
-#     int wins = 0 # 0 <= wins <= visits
-#
-#     int gameover_visits = 0
-#     int gameover_wins = 0
-#
-#     int height = height #if root is first state in game MCTS saw
-#
-#     int threads_checking_node = 0
-#     float UCT_multiplier = 1
-#     int num_children_being_checked = 0
-
-    #
-    #
-    #
-    #
-    #
-    #
-    # int overwhelming_amount = 1#65536
-    #
-    # int num_to_consider = 1
-    # int num_to_keep = 1
-    # int times_reexpanded = 0
-
+"""'
+TreeNode dict. 
+Holds the relevant info for a node in the game tree
+''"""
 cpdef dict TreeNode(dict game_board, list white_pieces, list black_pieces, str player_color, int index, dict parent, int height):
     cdef: #redundant
         int visits = 0
